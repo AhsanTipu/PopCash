2628a47daaabde0da4bbafbba0dde996fbdde4c3
AhsanTipu <syedahsan2305@gmail.com>
2021-05-26 04:23:31 +0500

    POPCahs Updated

---
+++ css/js/script.js
@@ -0,0 +1,28 @@
+const pan1=()=>{
+    document.getElementById("main1").style.display="none";
+    document.getElementById("main2").style.display="block";
+    document.getElementById("main3").style.display="none";
+    document.getElementById("main4").style.display="none";
+
+
+}
+const pan2=()=>{
+    document.getElementById("main1").style.display="none";
+    document.getElementById("main2").style.display="none";
+    document.getElementById("main3").style.display="block";
+    document.getElementById("main4").style.display="none";
+
+
+}
+const pan3=()=>{
+    document.getElementById("main1").style.display="none";
+    document.getElementById("main2").style.display="none";
+    document.getElementById("main3").style.display="none";
+    document.getElementById("main4").style.display="block";
+}
+const pan4=()=>{
+    document.getElementById("main1").style.display="none";
+    document.getElementById("main2").style.display="none";
+    document.getElementById("main3").style.display="block";
+    document.getElementById("main4").style.display="none";
+}
